b8adc86f7a540293aa3372ccbbb7263340d1b9ea
taihandsome19 <tai.tranthanh@hcmut.edu.vn>
2024-09-11 15:12:50 +0700

    Update landing page

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,9 +1,13 @@
 import NotFoundPage from "../components/NotFoundPage/NotFoundPage"
+import LandingPage from "../pages/LangdingPage/LandingPage"
 import HomePage from "../pages/HomePage/HomePage"
 
 export const routes = [
     {
         path: '/',
+        page: LandingPage,
+    },{
+        path: '/home',
         page: HomePage,
         isShowHeader: true,
         isShowFooter: true,
