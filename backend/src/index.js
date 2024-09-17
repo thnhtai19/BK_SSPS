ebe36dbf01cf8537b61c95fbe9be519f1e5490b2
Nhnhanbt <nhnhanbnt@gmail.com>
2024-09-17 13:39:05 +0700

    Base BE

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,19 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3001;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: "http://localhost:3000",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(cookieParser());
 
 routes(app);
 
