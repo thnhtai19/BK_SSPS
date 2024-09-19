b6f29be0ab72e499d2c33ea8ac0bb02522e3cb7d
tcong <tcongdang04@gmail.com>
2024-09-20 02:47:22 +0700

    Add HistoryPage, HomePage, ServicePage (1)

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,18 +1,31 @@
-import NotFoundPage from "../pages/NotFoundPage/NotFoundPage"
-import LandingPage from "../pages/LandingPage/LandingPage"
-import HomePage from "../pages/HomePage/HomePage"
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+import LandingPage from '../pages/LandingPage/LandingPage';
+import HomePage from '../pages/HomePage/HomePage';
+import HistoryPage from '../pages/HistoryPage/HistoryPage';
+import ServicePage from '../pages/ServicePage/ServicePage';
 
 export const routes = [
-    {
-        path: '/',
-        page: LandingPage,
-    },{
-        path: '/home',
-        page: HomePage,
-        isShowDashboard: true,
-    },
-    {
-        path: '*',
-        page: NotFoundPage
-    }
-]
+	{
+		path: '/',
+		page: LandingPage,
+	},
+	{
+		path: '/home',
+		page: HomePage,
+		isShowDashboard: true,
+	},
+	{
+		path: '*',
+		page: NotFoundPage,
+	},
+	{
+		path: '/history',
+		page: HistoryPage,
+		isShowDashboard: true,
+	},
+	{
+		path: '/service',
+		page: ServicePage,
+		isShowDashboard: true,
+	},
+];
