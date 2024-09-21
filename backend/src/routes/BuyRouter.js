9440f00f72c2c1f7efdf98f4a0cf1e9aad1c8b3c
duongdong <duongdong3949@gmail.com>
2024-09-21 22:16:12 +0700

    update

--- backend/src/routes/BuyRouter.js
+++ backend/src/routes/BuyRouter.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/BuyController');
-router.put('/', controller.BuyPages);
+router.post('/', controller.createNewPurchaseOrder)
+router.post('/', controller.updatePages)
+module.exports = router;
