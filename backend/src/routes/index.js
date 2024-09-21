e53694771265e5634371a7d592d3cb6954a83efe
duongdong <duongdong3949@gmail.com>
2024-09-21 22:14:51 +0700

    update

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,13 @@
 const TestRouter = require('./TestRouter');
+const UserRouter = require('./UserRouter');
+const AuthRouter = require('./AuthRouter');
+const BuyRouter = require('./BuyRouter');
 
 const routes = (app) => {
     app.use('/test', TestRouter);
+    app.use('/auth', AuthRouter);
+    app.use('/user', UserRouter);
+    app.use('/buy', BuyRouter);
 }
 
 module.exports = routes;
