d7ab4a950168543224d3ffa4b56cfc39a5c0c63a
duongdong <duongdong3949@gmail.com>
2024-09-21 22:15:36 +0700

    update

--- backend/src/routes/UserRouter.js
+++ backend/src/routes/UserRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const controller = require('../controllers/UserController');
-router.get('/', controller.getUserInforById);
+router.get('/profile', controller.getUserInforById);
 module.exports = router
