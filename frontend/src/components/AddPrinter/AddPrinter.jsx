1d17d094ba6ea53a55c436ae44c4da557840e1a8
Sherllgen <sherllgen@gmail.com>
2024-10-23 18:13:05 +0700

    Edit add printer form

--- frontend/src/components/AddPrinter/AddPrinter.jsx
+++ frontend/src/components/AddPrinter/AddPrinter.jsx
@@ -80,70 +80,72 @@ const AddPrinter = ({ onClose, onAddPrinterSuccess }) => {
 						</button>
 					</div>
 					{errorMessage && <p className="text-red-500">{errorMessage}</p>}
-					<form className="flex sm:gap-10 gap-0 flex-wrap" onSubmit={handleSubmit}>
-						<div className="flex-col flex-1">
-							<FormInput 
-								ID={"printer-name"}
-								Type={"text"}
-								Text={"Tên máy in"}
-								initialValue={printerName}
-								onChange={(e) => setPrinterName(e)}
-							/>
-							<FormInput 
-								ID={"brand"}
-								Type={"text"}
-								Text={"Thương hiệu"}
-								initialValue={brand}
-								onChange={(e) => setBrand(e)}
-							/>
-							<FormInput 
-								ID={"printer-type"}
-								Type={"text"}
-								Text={"Đời máy in"}
-								initialValue={printerVersion}
-								onChange={(e) => setPrinterVersion(e)}
-							/>
-							<FormInput 
-								ID={"description"}
-								Type={"text"}
-								Text={"Mô tả"}
-								initialValue={description}
-								onChange={(e) => setDescription(e)}
-							/>
-						</div>
+					<form onSubmit={handleSubmit}>
+						<div className="flex sm:gap-10 gap-0 flex-wrap" >
+							<div className="flex-col flex-1">
+								<FormInput 
+									ID={"printer-name"}
+									Type={"text"}
+									Text={"Tên máy in"}
+									initialValue={printerName}
+									onChange={(e) => setPrinterName(e)}
+								/>
+								<FormInput 
+									ID={"brand"}
+									Type={"text"}
+									Text={"Thương hiệu"}
+									initialValue={brand}
+									onChange={(e) => setBrand(e)}
+								/>
+								<FormInput 
+									ID={"printer-type"}
+									Type={"text"}
+									Text={"Đời máy in"}
+									initialValue={printerVersion}
+									onChange={(e) => setPrinterVersion(e)}
+								/>
+								<FormInput 
+									ID={"description"}
+									Type={"text"}
+									Text={"Mô tả"}
+									initialValue={description}
+									onChange={(e) => setDescription(e)}
+								/>
+							</div>
 
-						<div className="flex-col flex-1">
-							{/* <legend className="text-center">Vị trí</legend> */}
-							<fieldset>
-								<FormInput 
-									ID={"co-so"}
-									Type={"text"}
-									Text={"Cơ sở"}
-									initialValue={co_so}
-									onChange={(e) => setCo_so(e)}
-								/>
-								<FormInput 
-									ID={"toa"}
-									Type={"text"}
-									Text={"Tòa"}
-									initialValue={toa}
-									onChange={(e) => setToa(e)}
-								/>
-								<FormInput 
-									ID={"phong"}
-									Type={"text"}
-									Text={"Phòng"}
-									initialValue={room}
-									onChange={(e) => setRoom(e)}
-								/>
-							</fieldset>
+							<div className="flex-col flex-1">
+								{/* <legend className="text-center">Vị trí</legend> */}
+								<fieldset>
+									<FormInput 
+										ID={"co-so"}
+										Type={"text"}
+										Text={"Cơ sở"}
+										initialValue={co_so}
+										onChange={(e) => setCo_so(e)}
+									/>
+									<FormInput 
+										ID={"toa"}
+										Type={"text"}
+										Text={"Tòa"}
+										initialValue={toa}
+										onChange={(e) => setToa(e)}
+									/>
+									<FormInput 
+										ID={"phong"}
+										Type={"text"}
+										Text={"Phòng"}
+										initialValue={room}
+										onChange={(e) => setRoom(e)}
+									/>
+								</fieldset>
+							</div>
 						</div>
-					</form>
 						<div className="flex justify-end  mt-4">
 							<button type="submit" className="bg-[#0688B4] mx-auto   text-white font-medium  py-1 w-3/5 shadow-inner  hover:shadow-[white] rounded-lg">
 								Thêm
 							</button>
 						</div>
+					</form>
 				</div>
 			</div>
 		</>
